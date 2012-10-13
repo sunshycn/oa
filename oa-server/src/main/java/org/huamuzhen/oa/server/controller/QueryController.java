d0b685f96549072370f916043992da7f1ff964dc
lliu161 <lyliu@walmart.com>
2012-10-13 18:38:19 +0800

    add controllers

---
+++ oa-server/src/main/java/org/huamuzhen/oa/server/controller/QueryController.java
@@ -0,0 +1,10 @@
+package org.huamuzhen.oa.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/query")
+public class QueryController {
+
+}
