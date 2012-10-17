9c6921ab9718022797d954947a1ccc34ed871c8b
Leon Liu <lyliu@walmart.com>
2012-10-17 18:10:40 +0800

    change variable name

--- domain/src/main/java/org/huamuzhen/oa/domain/enumeration/ReportFormStatus.java
+++ domain/src/main/java/org/huamuzhen/oa/domain/enumeration/ReportFormStatus.java
@@ -3,12 +3,12 @@ package org.huamuzhen.oa.domain.enumeration;
 public enum ReportFormStatus {
 	
 	NOT_SEND,
-	SEND_TO_ORG_UNITS,
+	SENT_TO_ORG_UNITS,
 	GOT_REPLY_FROM_UNITS,
-	SEND_TO_LEADER1,
-	SEND_TO_LEADER2,
-	SEND_TO_OFFICE,
-	SEND_BACK_TO_REPORT_UNIT,
+	SENT_TO_LEADER1,
+	SENT_TO_LEADER2,
+	SENT_TO_OFFICE,
+	PASSED,
 	DENIED
 	
 }
