4d52d4bc8abfa43cbcbf430c0bca5867963950ce
lliu161 <lyliu@walmart.com>
2012-10-20 10:46:45 +0800

    啊啊啊

--- oa-server/src/main/java/org/huamuzhen/oa/server/interceptor/AuthorizeInterceptor.java
+++ oa-server/src/main/java/org/huamuzhen/oa/server/interceptor/AuthorizeInterceptor.java
@@ -3,6 +3,7 @@ package org.huamuzhen.oa.server.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.huamuzhen.oa.domain.entity.User;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
@@ -10,8 +11,17 @@ public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-		System.out.println("###########INTERCEPTOR##########");
-		return super.preHandle(request, response, handler);
+	
+		User user = (User)request.getSession().getAttribute("currentUser");
+		if(user == null){
+			response.getWriter().write("失去和服务器联系，请重新登录");
+			response.getWriter().write("<br/>");
+			response.getWriter().write("<a href="+request.getContextPath()+">返回首页</a><br/>");
+			return false;
+		}else{
+			return super.preHandle(request, response, handler);
+		}
+		
 	}
 	
 
---
+++ oa-server/src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>错误</title>
+</head>
+<body>
+	<h2>出错了</h2>
+	${errorMessage} <br/>
+	<a href="${contextPath}/">返回首页</a><br/>
+</body>
+</html>
