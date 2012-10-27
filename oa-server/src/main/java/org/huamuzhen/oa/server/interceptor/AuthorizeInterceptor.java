e5656d4d934a200895b58ba8563d0a833aed71ea
leon.liu <leon.liu@BS-NOT-136.cn.bleum.com>
2012-10-28 07:31:00 +0800

    fix bug

--- oa-server/src/main/java/org/huamuzhen/oa/server/interceptor/AuthorizeInterceptor.java
+++ oa-server/src/main/java/org/huamuzhen/oa/server/interceptor/AuthorizeInterceptor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.huamuzhen.oa.domain.entity.User;
+import org.huamuzhen.oa.domain.enumeration.Privilege;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
@@ -12,17 +13,27 @@ public class AuthorizeInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 
-		if(request.getRequestURI().equals(request.getContextPath()) || request.getRequestURI().equals(request.getContextPath()+"/") || 
-				request.getRequestURI().equals(request.getContextPath()+"/login")){
-			return true;
-		}
-		
 		User user = (User)request.getSession().getAttribute("currentUser");
 		if(user == null){
-			request.getRequestDispatcher("/").forward(request, response);
+			response.setCharacterEncoding("utf-8");
+			response.getWriter().write("还未登陆，请登录");
 			return false;
 		}
-		return super.preHandle(request, response, handler);
+		if(user.getPrivilege()== Privilege.ADMIN){
+			if(request.getRequestURI().startsWith(request.getContextPath()+"/reportForm") || request.getRequestURI().startsWith(request.getContextPath()+"/feedback")){
+				response.setCharacterEncoding("utf-8");
+				response.getWriter().write("权限不够");
+				return false;
+			}
+		}else{
+			if(request.getRequestURI().startsWith(request.getContextPath()+ "/user") || request.getRequestURI().startsWith(request.getContextPath()+"/orgUnit") || request.getRequestURI().startsWith(request.getContextPath()+"/reportFormType")){
+				response.setCharacterEncoding("utf-8");
+				response.getWriter().write("权限不够");
+				return false;
+			}
+		}
+		
+		return true;
 	}
 	
 }
